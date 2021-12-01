c0e50fe6086b19f86aeb5af949effc22f4b1237b
ogomezalumno <ogomezalumno@gmail.com>
2021-12-02 00:39:30 +0100

    Añadido GetUsers

--- frontend/main.js
+++ frontend/main.js
@@ -1,33 +1,12 @@
 // Comprobamos si el navegador soporta web sockets
 if ("WebSocket" in window) {
   let socket = new WebSocket("wss://a6ytms7731.execute-api.eu-central-1.amazonaws.com/sandbox");
-
-  socket.onopen = function(event) {
-//        alert("[open] Connected");
-      };
-
-  socket.onerror = function(error) {
-        alert(`[error] ${error.message}`);
-      };
-
-  socket.onmessage = function(event) {
-        alert(`[message] Data from server: ${event.data}`);
-        render(event.data);
-      };
-
-  socket.onclose = function(event) {
-        if (event.wasClean) {
-          alert(`[close] Conexión cerrada, code=${event.code} source=${event.reason}`);
-        } 
-        else {  
-          alert('[Error] Conexión perdida');
-        }
-      };
+  var login = false;
+  var userid = "";
 
   function render(data) {
     var html = `<div>
-                <strong>${data}</strong>:
-                <em>render_info</em>
+                <strong>${data}</strong>
                 </div>`;
       
     document.getElementById("messages").innerHTML = html;
@@ -36,23 +15,68 @@ if ("WebSocket" in window) {
   function Login(event) {
     var message = {
       action: "user",
-      username: document.getElementById("username").value,
-      userid: 1
+      username: document.getElementById("username").value
     };
-    console.log(message);
+    // En proceso de registro en la bbdd
+    login = true;
     socket.send(JSON.stringify(message));
     document.getElementById("username").value = "";
     return false;
   }
 
-  function SendMessage(event) {
+  function GetUsers(event) {
     var message = {
-      text: document.getElementById("texto").value,
+      action: "getusers",
     };
-
-    //socket.emit("action", message);
+    socket.send(JSON.stringify(message));
+    document.getElementById("userlist").value = "";
     return false;
   }
+
+//  function SendMessage(event) {
+//    var message = {
+//      text: document.getElementById("texto").value,
+//    };
+
+    //socket.emit("action", message);
+//    return false;
+//  }
+
+  socket.onopen = function(event) {
+         userid = "";
+      };
+
+  socket.onerror = function(error) {
+        login = false; 
+        userid = "";
+        alert(`[error] ${error.message}`);
+      };
+
+  socket.onmessage = function(event) {
+        // Si nos estabamos registrando en la bbdd de la palicación...
+        if (login == true) {
+           // Informamos y recolectamos nuestro ID de sesión websockets
+           render("Conectado con ID: "+event.data);
+           userid = event.data;
+           GetUsers(event);
+           login = false;
+        }
+        else {
+           //alert(`[message] Data from server: ${event.data}`);
+           render(event.data);
+        }
+      };
+
+  socket.onclose = function(event) {
+        login = false; 
+        userid = "";
+        if (event.wasClean) {
+          alert(`[close] Conexión cerrada, code=${event.code} source=${event.reason}`);
+        } 
+        else {  
+          alert('[Error] Conexión perdida');
+        }
+      };
 } 
 else {          
   // The browser doesn't support WebSocket
